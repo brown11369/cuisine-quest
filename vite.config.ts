915c5e49a609895fa3bcd54cd4259861a119322a
Brown <63307642+brown11369@users.noreply.github.com>
2025-12-12 17:28:31 +0530

    feat: add testing and coverage scripts, update devDependencies, and configure Vitest in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+
   // esbuild: {
   //   jsxInject: `import React from 'react'`,
   // dirs:[]
   // },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+
+  // Vitest config goes here
+  test: {
+    environment: "jsdom",
+    globals: true,
   },
 });
